a85977a69476b068db44477c1d2ccdbc7faaac0b
surmon-china <surmon.me@gmail.com>
2016-05-07 23:55:36 +0800

    Init

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// 全局变量
-global.config = {
-  name: 'NodePress',
-  version: '0.0.1',
-  api_path: '/api',
-  author: 'Surmon',
-  site: 'http://surmon.me',
-  mongodb: {
-    host: '10.9.58.169',
-    name: 'cba84ab5-f3b1-4191-935b-e720db9a1218',
-    password: 'X3EMILtQV2FaBcNPmY54XQ',
-    port: '27017',
-    uri: 'mongodb://0d797cf7-5c73-43cf-a51b-434e062affa0:X3EMILtQV2FaBcNPmY54XQ@10.9.58.169:27017/cba84ab5-f3b1-4191-935b-e720db9a1218',
-    username: '0d797cf7-5c73-43cf-a51b-434e062affa0'
-  }
-};
+// 程序配置
+require('./np-config.js');
+
 // 路由
 require('./np-routes.js');
+
+//数据库
+require('./np-mongodb.js');
