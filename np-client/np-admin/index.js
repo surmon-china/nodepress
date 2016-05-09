0f191bfb57154b1a952e5b95d407ec34a3176582
surmon-china <surmon@foxmail.com>
2016-05-09 23:22:56 +0800

    Aside Init

--- np-client/np-admin/index.js
+++ np-client/np-admin/index.js
@@ -1,4 +1,3 @@
-
 // 全局数据常量
 const AdminConfig = {
 	name: 'NorePress',
@@ -51,6 +50,70 @@ angular.module('NodePress', [
 
 // Aside边栏控制器
 .controller('AsideController', ['$scope','$http','$rootScope','$location',function($scope,$http,$rootScope,$location) {
+
+  $scope.adide = {};
+
+  $scope.adide.menus = [{
+    name: '文章管理',
+    childrens: [{
+      name: '发布文章',
+      url: '/article/new'
+    },{
+      name: '所有文章',
+      url: '/article/all'
+    },{
+      name: '分类目录',
+      url: '/article/category'
+    },{
+      name: '文章标签',
+      url: '/article/tag'
+    }]
+  },{
+    name: '页面管理',
+    childrens: [{
+      name: '新建页面',
+      url: '/page/new'
+    },{
+      name: '所有页面',
+      url: '/page/all'
+    }]
+  },{
+    name: '菜单管理',
+    url: '/menus'
+  },{
+    name: '评论管理',
+    url: '/comments'
+  },{
+    name: '个人资料',
+    url: '/user'
+  },{
+    name: '主题管理',
+    url: '/themes'
+  },{
+    name: '文件管理',
+    url: '/files'
+  },{
+    name: '扩展管理',
+    url: '/plugins'
+  },{
+    name: '代码管理',
+    url: '/ide'
+  },{
+    name: '全局设置',
+    childrens: [{
+      name: '基本设置',
+      url: '/options/base'
+    },{
+      name: '高级设置',
+      url: '/options/senior'
+    },{
+      name: '其他设置',
+      url: '/options/others'
+    }]
+  },{
+    name: '自定义配置',
+    url: '/custom'
+  }];
 
 }])
 
--- np-config.js
+++ np-config.js
@@ -1,10 +1,6 @@
 // 全局变量
-global.config = {
-  name: 'NodePress',
-  version: '0.0.1',
-  api_path: '/api',
-  author: 'Surmon',
-  site: 'http://surmon.me',
+global.NP_CONFIG = {
+  api: '/api',
   mongodb: {
     host: '10.9.58.169',
     name: 'cba84ab5-f3b1-4191-935b-e720db9a1218',
@@ -12,5 +8,12 @@ global.config = {
     port: '27017',
     uri: 'mongodb://0d797cf7-5c73-43cf-a51b-434e062affa0:X3EMILtQV2FaBcNPmY54XQ@10.9.58.169:27017/cba84ab5-f3b1-4191-935b-e720db9a1218',
     username: '0d797cf7-5c73-43cf-a51b-434e062affa0'
+  },
+  info: {
+    name: 'NodePress',
+    version: '0.0.1',
+    api: '/api',
+    author: 'Surmon',
+    site: 'http://surmon.me',
   }
 };
