3dc0673e8352bf068eb5f5b795c944a844517763
surmon-china <surmon@foxmail.com>
2016-05-11 22:35:20 +0800

    Init

---
+++ np-client/np-admin/scripts/controllers/article-controller.js
@@ -0,0 +1,12 @@
+/*
+* ArticleController Module
+*
+* Description
+*/
+angular.module('ArticleController', [])
+.controller('ArticleController', ['$scope', '$rootScope', '$modal', '$timeout', '$location', '$localStorage', 'appConfig',
+  function ($scope, $rootScope, $modal, $timeout, $location, $localStorage, appConfig) {
+
+    console.log('ArticleController');
+  }
+]);
---
+++ np-client/np-admin/scripts/controllers/page-controller.js
@@ -0,0 +1,12 @@
+/*
+* PageController Module
+*
+* Description
+*/
+angular.module('PageController', [])
+.controller('PageController', ['$scope', '$rootScope', '$modal', '$timeout', '$location', '$localStorage', 'appConfig',
+  function ($scope, $rootScope, $modal, $timeout, $location, $localStorage, appConfig) {
+
+    console.log('PageController');
+  }
+]);
