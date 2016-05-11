5d011ca2b3700d2b2c60f5b1fe2290364fed8bff
surmon <surmon@winhu.com>
2016-05-11 22:35:20 +0800

    Init

--- np-client/np-admin/index.js
+++ np-client/np-admin/index.js
@@ -14,6 +14,114 @@ angular.module('NodePress', [
   'ngStorage',
 ])
 
+// 主程序入口模块
+angular.module('app', [
+
+  // angular modules
+  'ui.router',
+  'ngStorage',
+  'ngResource',
+  'ngSanitize',
+  'ngAnimate',
+
+  // angular routes
+  'AppRoutes',
+
+  // third modules
+  'angular.modal',
+  'angular.loading',
+
+  // angular directive
+  'TabsDirective',
+  'TooltipDirective',
+  'PaginationDirective',
+  'LoadingDirective',
+
+  // Global controller
+  'MainController',
+  'ArticleController',
+  'HeaderController',
+  'AuthController',
+
+  // Function controller
+  'IndexController',
+  'CourseController',
+  'PaymentController',
+  'OrganizationlController',
+  'UserController',
+  'StudentController',
+  'IncomeController',
+  'LessonController',
+  'TeacherController',
+  'SchoolController',
+
+  // Global Filter
+  'HtmlFilter',
+  'TimeFilter',
+
+  // Global package
+  'CommonProvider',
+  'AuthProvider',
+  'QuploadProvider',
+
+  // Global Service
+  'AuthService',
+  'CommonService',
+
+  // Global Model
+  'CommonModel'
+])
+
+// 全局配置
+.provider('appConfig', function() {
+  var config = {
+    baseUrl: 'http://xtx.com',
+    fileUrl: 'http://7xnbft.com2.z0.glb.qiniucdn.com/',
+    staticUrl: 'http://7xpb5v.com2.z0.glb.qiniucdn.com',
+    apiUrl: '/server/api/v1'
+  };
+  return {
+    config:config,
+    $get: function() {
+      return config;
+    }
+  }
+})
+
+// 初始化
+.run(['AuthProvider', 'CommonProvider', function(AuthProvider, CommonProvider) {
+  // 自动登陆
+  AuthProvider.init();
+  // 自动回顶
+  CommonProvider.autoTop();
+}])
+
+.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+  cfpLoadingBarProvider.includeSpinner = false;
+  cfpLoadingBarProvider.includeBar = false;
+  cfpLoadingBarProvider.latencyThreshold = 10;
+}])
+
+.config(function ($validationProvider, appConfigProvider) {
+  // 表单验证配置
+  angular.extend($validationProvider, {
+    validCallback: function (element){
+      $(element).parents('.form-group:first').removeClass('has-error').addClass('has-success');
+    },
+    invalidCallback: function (element) {
+      $(element).parents('.form-group:first').addClass('has-error');
+    }
+  });
+
+  $validationProvider.setErrorHTML(function (msg) {
+    return  "<i class=\"icon icon-error\"></i>" + msg;
+  });
+
+  $validationProvider.setSuccessHTML(function (msg) {
+    return  "<i class=\"icon icon-success\"></i>" + msg;
+  });
+});
+
 // 过滤器模块------------------------------------------------------------------------------------------
 
 // 路由模块--------------------------------------------------------------------------------------------
---
+++ np-client/np-admin/scripts/controllers/option-controller.js
@@ -0,0 +1,12 @@
+/*
+* OptionController Module
+*
+* Description
+*/
+angular.module('OptionController', [])
+.controller('OptionController', ['$scope', '$rootScope', '$modal', '$timeout', '$location', '$localStorage', 'appConfig',
+  function ($scope, $rootScope, $modal, $timeout, $location, $localStorage, appConfig) {
+
+    console.log('OptionController');
+  }
+]);
