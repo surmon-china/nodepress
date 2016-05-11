27a6f92c091bb326c65f189c387db3fcf19f1c5f
surmon-china <surmon.me@gmail.com>
2016-05-11 22:35:20 +0800

    Init

---
+++ np-client/np-admin/scripts/controllers/aside-controller.js
@@ -0,0 +1,12 @@
+/*
+* AsideController Module
+*
+* Description
+*/
+angular.module('AsideController', [])
+.controller('AsideController', ['$scope', '$rootScope', '$modal', '$timeout', '$location', '$localStorage', 'appConfig',
+  function ($scope, $rootScope, $modal, $timeout, $location, $localStorage, appConfig) {
+
+    console.log('AsideController');
+  }
+]);
