842bcaa43fb08cb5bc12b1c53d684b1265265f89
Surmon <surmon@winhu.com>
2016-05-21 18:05:36 +0800

    Init

--- np-client/np-admin/scripts/controllers/article-controller.js
+++ np-client/np-admin/scripts/controllers/article-controller.js
@@ -4,8 +4,8 @@
 * Description
 */
 angular.module('ArticleController', [])
-.controller('ArticleController', ['$scope', '$rootScope', '$modal', '$timeout', '$location', '$localStorage', 'appConfig',
-  function ($scope, $rootScope, $modal, $timeout, $location, $localStorage, appConfig) {
+.controller('ArticleController', ['$scope', '$rootScope', '$location', '$localStorage', 'AdminConfig',
+  function ($scope, $rootScope, $modal, $localStorage, AdminConfig) {
 
     console.log('ArticleController');
   }
