7f570acbd30122e24246490d8c07a621f9d642e4
surmon-china <surmon@foxmail.com>
2016-07-11 22:47:12 +0800

    OK

---
+++ np-route/view.js
@@ -0,0 +1,19 @@
+
+// 视图模板路由
+
+exports.index = function(req, res){
+  // res.render('index');
+  res.sendfile('./np-public/np-theme/Surmon/index.html');
+};
+
+exports.admin = function(req, res){
+  // res.render('index');
+  res.sendfile('./np-public/np-admin/index.html');
+};
+
+/*
+exports.partials = function (req, res) {
+  var name = req.params.name;
+  res.render('partials/' + name);
+};
+*/
