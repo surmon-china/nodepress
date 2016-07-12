5b18e90906ff28536061d48c2a12ef7033d9637b
surmon-china <surmon.me@gmail.com>
2016-07-12 18:30:55 +0800

    OK

--- np-model/model.mongodb.js
+++ np-model/model.mongodb.js
@@ -1,17 +1,4 @@
-// 数据库
-var mongodb = require('mongodb');
-
-var server = new mongodb.Server('localhost', 27017, { safe: true });
-new mongodb.Db('local', server, {}).open(function(error, client){
-  if(error) throw error;
-  console.log('数据库连接成功');
-  var collection = new mongodb.Collection(client, 'config');
-  collection.find(function(error, cursor){ 
-    console.log(cursor);
-  //     cursor.each(function(error,doc){
-  //         if(doc){
-  //             console.log('name:' + doc.name + ' age:' + doc.age);
-  //         }
-      // });
-  });
-});
+// mongoose 链接
+var mongoose = require('mongoose');
+console.log(mongoose);
+// var db       = mongoose.createConnection('mongodb://127.0.0.1:27017/NodeJS'); 
