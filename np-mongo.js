6d2d49c0d97fb55d7bbcaef95fafae553cba1984
surmon-china <surmon.me@gmail.com>
2016-07-14 23:02:48 +0800

    Mongodb Optimize

--- np-mongo.js
+++ np-mongo.js
@@ -1,7 +1,8 @@
 // mongoose 数据库
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
-var mongodb  = mongoose.createConnection('mongodb://localhost:27017/NodePress');
+mongoose.connect("mongodb://localhost/NodePress");
+var mongodb = mongoose.connection;
 
 // console.log(mongodb);
 
@@ -15,8 +16,6 @@ mongodb.once('open', function() {
 
   console.log('mongoose 连接成功!');
 
-  // console.log(mongodb.collection('article'));
-
   // 定义一个集合/表
   var articleSchema = new Schema({
     title:  String,
@@ -26,13 +25,11 @@ mongodb.once('open', function() {
     date: { type: Date, default: Date.now },
     hidden: Boolean,
     sidebar: String,
-    meta: {
+    meta: { 
       views: Number,
       favs:  Number
     }
   });
-
-  // console.log(articleSchema);
 
   var Article = mongoose.model('article', articleSchema);
 
@@ -42,41 +39,23 @@ mongodb.once('open', function() {
     content: 'the body of my blog. can you see that?'        
   });
 
-  // console.log(Article);
-  // console.log(Article.findOne);
+  Article.find(function (err, configs) {
+    if (err) return console.error(err);
+    console.log(configs)
+  });
 
-  // Article.find({}, function (err, data) {
-  //   console.log(data);
-  // });
+  console.log('执行了');
+  article.save(function (err, doc) {
+    // if (err) // ...
+    console.log('meow');
+    console.log(doc);
+  });
 
-  // console.log(Article);
-  // console.log('保存数据');
+  Article.find(function (err, configs) {
+    if (err) return console.error(err);
+    console.log(configs)
+  });
 
-  var find = function () {
-    console.log('执行了');
-    Article.find({}).exec(function (err, todos){
-      err && console.log('find failed');
-      err || console.log('find success');
-    });
-  };
-
-  find();
-
-  // Article.find(function (err, configs) {
-  //   if (err) return console.error(err);
-  //   console.log(configs)
-  // });
-
-  // Config.findOne({name:"WangEr"}, function(err, doc){
-  //   if(err) console.log(err);
-  //   else console.log(doc.name + ", password - " + doc.password);
-  // });
-
-  // var lisi = new Config({name:"LiSi", password:"123456"});
-  // lisi.save(function(err, doc){
-  //   if(err)console.log(err);
-  //   else console.log(doc.name + ' saved');
-  // });
 });
 
 exports.mongodb = mongodb;
