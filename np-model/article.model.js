dbcd29c8a7446b9a8b0a20e744cf5e4891edd8d7
surmon-china <surmon.me@gmail.com>
2016-08-01 18:03:03 +0800

    Update

--- np-model/article.model.js
+++ np-model/article.model.js
@@ -1,5 +1,14 @@
+/*
+*
+* 文章数据模型
+*
+*/
+
 var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
+var mongoosePaginate = require('mongoose-paginate');
+
+// 自增ID初始化
 autoIncrement.initialize(mongoose.connection);
 
 // 文章集合模型
@@ -14,56 +23,49 @@ var articleSchema = new mongoose.Schema({
   // 文章描述
   description: String,
 
-  // 文章发布状态 =》 -1已删除，0草稿，1发布
+  // 文章别名（具有唯一性）
+  slug: { type: String, required: true },
+
+  // 文章发布状态 => -1已删除，0草稿，1已发布
   status: { type: Number, default: 1 },
 
   // 文章公开状态 = // 0非公开，1公开
   public: { type: Number, default: 1 },
 
-  // 文章密码 =》 加密状态生效
+  // 文章密码 => 加密状态生效
   password: String,
 
   // 发布日期
   date: { type: Date, default: Date.now },
 
   // 文章标签
-  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
+  tag: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
 
   // 文章分类
-  category: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
+  category: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }],
 
-  // 评论
-  comments: [{ 
-    content: String,
-    date: Date,
-    user: {
-      name: { type: String, default: '匿名用户' }, // 发布者名字
-      email: String, // 发布者邮箱
-      ip: { type: String, default: '未知IP' }, // 发布者IP
-      url: String, // 发布者地址
-      agent: String, // 发布者UA
-      reply: {} // 评论回复
-    }
-  }],
-
-  // 边栏展示 =》 0不显示，1left，2right
-  sidebar: String,
+  // 边栏展示 => 0不显示，1left，2right
+  sidebar: { type: String, default: 2 },
   meta: {
-    views: Number,
-    favs:  Number
+    views: { type: Number, default: 0 },
+    favs:  { type: Number, default: 0 },
+    comments: { type: Number, default: 0 },
   },
 
   // 自定义扩展
   extend: {}
 });
 
-//自增ID配置
+// 翻页 + 自增ID插件配置
+articleSchema.plugin(mongoosePaginate);
 articleSchema.plugin(autoIncrement.plugin, {
   model: 'Article',
   field: 'id',
   startAt: 1,
   incrementBy: 1
 });
+
+// 自增ID配置
 articleSchema.pre('save', next => {
   if (this.isNew) this.create_time = this.update_time = Date.now();
   if (!this.isNew) this.update_time = Date.now();
