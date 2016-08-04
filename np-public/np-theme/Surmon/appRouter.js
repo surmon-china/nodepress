2937c232365f54a82048c7c25cb076889bae2149
surmon-china <surmon@foxmail.com>
2016-08-04 21:26:41 +0800

    VUE Update

---
+++ np-public/np-theme/Surmon/appRouter.js
@@ -0,0 +1,61 @@
+/*
+*
+* 路由模块
+*
+*/
+
+// Index
+import IndexView from './components/index/IndexView.vue'
+
+// Articles
+// import ArticleView from './components/article/ListView.vue'
+
+export const RouterMap = router => {
+  router.map({
+    '/': {
+      name: 'index',
+      component: IndexView
+    },
+    /*
+    '/article': {
+      component: ArticleView
+      component: {
+        template: '<p>Hello from /b</p>'
+        template: '<router-view></router-view>'
+      },
+      subRoutes: {
+        // 当路径是 /article/:article_id 时进行渲染
+        '/:article_id': { component: { template: 'D' }},
+        // 当路径是 /c/e 时进行渲染
+        '/e': { component: { template: 'E' }}
+      }
+    }
+    */
+  })
+
+  // 路由重定向
+  router.redirect({
+    '*': '/'
+  })
+
+  // 路由每次变更
+  router.beforeEach(() => {
+
+    /*
+    window.scrollTo(0, 0)
+
+    // 登录中间验证，页面需要登录而没有登录的情况直接跳转登录
+    if (transition.to.auth) {
+      if (localStorage.userId) {
+          transition.next();
+      } else {
+          var redirect = encodeURIComponent(transition.to.path);
+          transition.redirect('/login?redirect=' + redirect);
+      }
+    } else {
+      transition.next();
+    }
+
+    */
+  })
+}
---
+++ np-public/np-theme/Surmon/components/layout/FooterView.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer">
+    <div>我是网站底部</div>
+  </footer>
+</template>
+
+<style lang="sass" scoped>
+</style>
