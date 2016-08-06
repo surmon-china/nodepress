576c61c8d70279624905e98d04da5dfcc1f445e2
surmon <surmon@winhu.com>
2016-08-06 21:12:37 +0800

    OK

--- np-public/np-theme/Surmon/App.vue
+++ np-public/np-theme/Surmon/App.vue
@@ -1,23 +1,32 @@
 <style lang="stylus">
-
   @import './stylus/variables'
-
   body
     padding-top 4em
     
+    .container
+      position relative
+    
+    article
+      width 40em
+      min-height 40em
+      margin-left 11em
+      padding-top 1em
 </style>
 
 <template>
     <header-view></header-view>
     <div id="main" class="main">
-      <article class="article">
-        <router-view  class="router-view"
-                      keep-alive
-                      transition
-                      transition-mode="out-in">
-        </router-view>
-      </article>
-      <aside-view></aside-view>
+      <div class="container">
+        <nav-view></nav-view>
+        <article class="article">
+          <router-view  class="router-view"
+                        keep-alive
+                        transition
+                        transition-mode="out-in">
+          </router-view>
+        </article>
+        <aside-view></aside-view>
+      </div>
     </div>
     <footer-view></footer-view>
 </template>
@@ -28,6 +37,7 @@
   import HeaderView from './components/layout/HeaderView.vue'
   import FooterView from './components/layout/FooterView.vue'
   import AsideView from './components/layout/AsideView.vue'
+  import NavView from './components/layout/NavView.vue'
 
   // 模块配置
   export default {
@@ -36,7 +46,8 @@
     components: {
       HeaderView,
       FooterView,
-      AsideView
+      AsideView,
+      NavView
     }
   }
 
