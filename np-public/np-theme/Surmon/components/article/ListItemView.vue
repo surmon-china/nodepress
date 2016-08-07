f683bf0efee021abb72eff472452615555754bc0
surmon-china <surmon@foxmail.com>
2016-08-07 23:23:24 +0800

    Update

--- np-public/np-theme/Surmon/components/article/ListItemView.vue
+++ np-public/np-theme/Surmon/components/article/ListItemView.vue
@@ -1,35 +1,98 @@
 <style lang="stylus">
+  @import '../../stylus/variables'
+  .article-list-item
+    border-bottom 1px dashed $gray-lighter
+    // margin-bottom 1em
+    // padding 1em
+    
+    .item-content
+      display block
+      overflow hidden
+      height 8.5em
+      padding 1em
+      
+      &:hover
+        // background-color: $gray-lightsuper
+        // .item-title
+        
+      .item-thumb
+        float left
+        width 9em
+        
+        .item-thumb-img
+          max-width 100%
+          height 6.5em
+      
+      .item-body
+        float right
+        width 28em
+        height 6.5em
+        padding-top .2em
+        
+        .item-title
+          // padding .3em .1em
+          // padding-top .3em
+          font-weight bold
+          text-truncate()
+          
+        .item-description
+          font-size .9em
+          margin 0
+          margin-bottom 0.3em
+          display -webkit-box
+          display -moz-box
+          overflow hidden
+          text-overflow ellipsis
+          word-break break-all
+          -webkit-box-orient vertical
+          -webkit-line-clamp 2
+          
+        .item-meta
+          font-size .9em
+          height 1.6em
+          line-height 2em
+          
+          span
+            margin-right .5em
 </style>
 
 <template>
-  <div class="article-list-item" v-show="item">
-
-    <a href="#">
-      <div class="media">
-        <div class="media-left media-middle">
-            <img class="media-object" data-src="holder.js/64x64" alt="Generic placeholder image">
-        </div>
-        <div class="media-body">
-          <h4 class="media-heading">文章标题</h4>
-          <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-          <p>Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+  <div class="article-list-item">
+    <a v-link="{ path: '/article/'+ item.slug }" class="item-content">
+      <div class="item-thumb">
+          <img class="item-thumb-img" 
+               src="http://surmon.me/wp-content/themes/Surmon/timthumb.php?src=http://surmon.me/wp-content/uploads/2015/11/20120815211519_LZsce.thumb_.600_0.jpeg&h=96&w=144&q=100&zc=1" 
+               alt="Generic placeholder image">
+      </div>
+      <div class="item-body">
+        <h5 class="item-title">{{ item.title }}</h5>
+        <p class="item-description">{{ item.description }}</p>
+        <div class="item-meta">
+          <span class="date">
+            <i class="iconfont icon-clock"></i>
+            <span>{{ item.date }}</span>
+          </span>
+          <span class="views">
+            <i class="iconfont icon-eye"></i>
+            <span>{{ item.meta.views }}</span>
+          </span>
+          <span class="comment">
+            <i class="iconfont icon-comment"></i>
+            <span>{{ item.meta.comments }}</span>
+          </span>
+          <span class="tag">
+            <i class="iconfont icon-tag"></i>
+            <span v-if="!item.tag.length">无</span>
+            <span v-for="tag in item.tag">{{ tag.name }}</span>
+          </span>
+          <span class="category">
+            <i class="iconfont icon-list"></i>
+            <span v-if="!item.category.length">未分类</span>
+            <span v-for="category in item.category">{{ category.name }}</span>
+          </span>
         </div>
       </div>
     </a>
-    <!-- <p class="itemtext" v-if="hasText" v-html="item.text"></p>
-    <ul class="poll-options" v-if="pollOptions">
-      <li v-for="option in pollOptions">
-        <p>{{option.text}}</p>
-        <p class="subtext">{{option.score}} points</p>
-      </li>
-    </ul>
-    <ul class="comments" v-if="comments">
-      <comment
-        v-for="comment in comments"
-        :comment="comment">
-      </comment>
-    </ul>
-    <p v-show="!comments.length && !isJob">No comments yet.</p> -->
   </div>
 </template>
 
@@ -39,42 +102,19 @@ export default {
 
   name: 'ListItemView',
 
-  components: {
-    // Item,
-    // Comment
-  },
-
-  data () {
-    return {
-      item: {},
-      comments: [],
-      pollOptions: null
-    }
-  },
-
-  route: {
-    data ({ to }) {
-      // return store.fetchItem(to.params.id).then(item => {
-      //   document.title = item.title + ' | Vue.js HN Clone'
-      //   return {
-      //     item,
-      //     // the final resolved data can further contain Promises
-      //     comments: store.fetchItems(item.kids),
-      //     pollOptions: item.type === 'poll'
-      //       ? store.fetchItems(item.parts)
-      //       : null
-      //   }
-      // })
-    }
+  props: {
+    item: Object
   },
 
   computed: {
-    // isJob () {
-    //   return this.item.type === 'job'
+    // href () {
+    //   return this.item.url || ('#/item/' + this.item.id)
     // },
-
-    // hasText () {
-    //   return this.item.hasOwnProperty('text')
+    // showInfo () {
+    //   return this.item.type === 'story' || this.item.type === 'poll'
+    // },
+    // showDomain () {
+    //   return this.item.type === 'story'
     // }
   }
 }
