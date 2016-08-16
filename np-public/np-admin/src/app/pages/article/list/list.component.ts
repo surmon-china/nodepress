1b03b5b706da43f480e0d1b8f715edb2e6418767
surmon-china <surmon@foxmail.com>
2016-08-16 23:55:57 +0800

    ng2 http

--- np-public/np-admin/src/app/pages/article/list/list.component.ts
+++ np-public/np-admin/src/app/pages/article/list/list.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {BaCard} from '../../../theme/components/baCard';
+import {ArticleService} from '../../../theme/services/article';
 
 @Component({
   selector: 'list',
@@ -8,6 +9,59 @@ import {BaCard} from '../../../theme/components/baCard';
 })
 
 export class ArticleList {
+
   constructor() {
+    this.articles = {};
+  }
+
+  public getArticles = params => {
+    console.log('hello');
+    let service = new ArticleService().getLists({ page: 1 });
+    // service.then(articles => {
+    //   this.articles = articles;
+    //   console.log(this);
+    // }).catch(error => {
+    //   console.log(error);
+    // });
+  };
+
+  ngOnInit() {
+    console.log('init');
+    // 初始化列表
+    this.getArticles()
+  }
+
+  ngOnDestroy() {
+    console.log('销毁');
+  }
+
+  ngDoCheck() {
+    // Custom change detection
+  }
+
+  ngOnChanges(changes) {
+    // Called right after our bindings have been checked but only
+    // if one of our bindings has changed.
+    //
+    // changes is an object of the format:
+    // {
+    //   'prop': PropertyUpdate
+    // }
+  }
+
+  ngAfterContentInit() {
+    // Component content has been initialized
+  }
+
+  ngAfterContentChecked() {
+    // Component content has been Checked
+  }
+
+  ngAfterViewInit() {
+    // Component views are initialized
+  }
+
+  ngAfterViewChecked() {
+    // Component views have been checked
   }
 }
