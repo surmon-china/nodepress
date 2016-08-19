96df5d9e7e0c0badf00dbac2365722c35e04b66d
surmon <surmon@winhu.com>
2016-08-20 01:54:51 +0800

    Service Http Provider OK

--- np-public/np-admin/src/app/index.ts
+++ np-public/np-admin/src/app/index.ts
@@ -1,8 +1,10 @@
 // App
-import {AppState} from "./app.state";
+import { AppState } from "./app.state";
+import { HTTP_PROVIDERS } from '@angular/http';
 export * from './app.component';
 
 // Application wide providers
 export const APP_PROVIDERS = [
-  AppState
+  AppState,
+  HTTP_PROVIDERS
 ];
