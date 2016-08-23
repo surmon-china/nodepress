b23de1baeeca6ceebdeea288e726d7ad2be4712a
surmon <surmon@winhu.com>
2016-08-23 23:24:59 +0800

    文章发布部分 完善

--- np-public/np-admin/src/app/pages/article/edit/edit.component.ts
+++ np-public/np-admin/src/app/pages/article/edit/edit.component.ts
@@ -5,12 +5,11 @@ import { ArticleContent } from './components/content';
 import { ArticleDuoshuo } from './components/duoshuo';
 import { ArticleSubmit } from './components/submit';
 import { ArticleThumb } from './components/thumb';
-import { ArticleTag } from './components/tag';
 
 
 @Component({
   selector: 'edit',
-  directives: [BaCard, ArticleCategory, ArticleContent, ArticleDuoshuo, ArticleSubmit, ArticleThumb, ArticleTag],
+  directives: [BaCard, ArticleCategory, ArticleContent, ArticleDuoshuo, ArticleSubmit, ArticleThumb],
   template: require('./edit.html')
 })
 
