70c3d219e21ef59014d2a241380bc1b16531396d
surmon-china <surmon.me@gmail.com>
2016-09-23 10:45:35 +0800

    前后端项目分离

--- np-public/np-admin/src/app/pages/editors/components/ckeditor/ckeditor.loader.ts
+++
@@ -1,3 +0,0 @@
-window['CKEDITOR_BASEPATH'] = '//cdn.bootcss.com/ckeditor/4.5.9/';
-// window['CKEDITOR_BASEPATH'] = '//cdn.ckeditor.com/4.5.9/standard/';
-require('ckeditor');
--- np-public/np-admin/src/app/theme/services/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './baImageLoader';
-export * from './baThemePreloader';
-export * from './baThemeSpinner';
