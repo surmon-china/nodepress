d9555b31a2f91d8bda54f6a6718731821a951520
surmon-china <surmon.me@gmail.com>
2017-02-02 21:01:35 +0800

    update

--- np-routes.js
+++ np-routes.js
@@ -10,7 +10,7 @@ const routes = app => {
 
     // Set Header
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("Content-Type", "application/json;charset=utf-8");
     res.header("X-Powered-By", 'Nodepress 1.0.0');
@@ -46,10 +46,11 @@ const routes = app => {
       return false;
     };
 
-    // 拦截请求
-    if (!Object.is(req.method, 'GET')  && 
-        !(Object.is(req.method, 'POST') && Object.is(req.url, '/auth')) &&
-        !isVerified()) {
+    // 拦截所有非管路员的非get请求（排除auth的post请求）
+    if (!isVerified() &&
+        !Object.is(req.method, 'GET')  && 
+        !(Object.is(req.method, 'POST') && Object.is(req.url, '/auth'))
+       ) {
       res.status(401).jsonp({ code: 0, message: '来者何人！' })
       return false;
     }
