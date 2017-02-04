754cc266dffe90539abf1fa67ff75b68d14d72e3
surmon-china <surmon@foxmail.com>
2017-02-05 03:38:07 +0800

    optimize code

--- np-routes.js
+++ np-routes.js
@@ -2,7 +2,7 @@
 
 const config = require('./np-config');
 const controller = require('./np-controller');
-const jwt = require('jsonwebtoken');
+const authIsVerified = require('./np-auth');
 const routes = app => {
 
   // 拦截器
@@ -13,7 +13,7 @@ const routes = app => {
     const origin = req.headers.origin || '';
     if (allowedOrigins.includes(origin) || origin.includes('localhost')) {
       res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    };
     res.header("Access-Control-Allow-Headers", "Authorization, Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("Content-Type", "application/json;charset=utf-8");
@@ -23,41 +23,16 @@ const routes = app => {
     if (req.method == 'OPTIONS') {
       res.send(200);
       return false;
-    }
-
-    // 验证Auth
-    const authToken = req => {
-      if (req.headers && req.headers.authorization) {
-        const parts = req.headers.authorization.split(' ');
-        if (Object.is(parts.length, 2) && Object.is(parts[0], 'Bearer')) {
-          return parts[1];
-        }
-      }
-      return false;
-    };
-
-    // 验证权限
-    const isVerified = () => {
-      const token = authToken(req);
-      if (token) {
-        try {
-          const decodedToken = jwt.verify(token, config.AUTH.jwtTokenSecret);
-          if (decodedToken.exp > Math.floor(Date.now() / 1000)) {
-            return true;
-          }
-        } catch (err) {}
-      }
-      return false;
     };
 
     // 拦截所有非管路员的非get请求（排除auth的post请求）
-    if (!isVerified() &&
+    if (!authIsVerified(req) &&
         !Object.is(req.method, 'GET')  && 
         !(Object.is(req.method, 'POST') && Object.is(req.url, '/auth'))
        ) {
       res.status(401).jsonp({ code: 0, message: '来者何人！' })
       return false;
-    }
+    };
 
     next();
   });
