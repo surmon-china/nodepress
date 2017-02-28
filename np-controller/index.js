f49351b8fa1a0ee46aea5145f380059ee75a9210
surmon-china <surmon@foxmail.com>
2017-03-01 02:14:40 +0800

    完善中

--- np-controller/index.js
+++ np-controller/index.js
@@ -1,11 +1,12 @@
 
 // API分发至控制器
 exports.tag = require('./tag.controller');
+exports.like = require('./like.controller');
 exports.auth = require('./auth.controller');
 exports.qiniu = require('./qiniu.controller');
 exports.option  = require('./option.controller');
 exports.sitemap = require('./sitemap.controller');
-// exports.comment  = require('./comment.controller');
+exports.comment  = require('./comment.controller');
 exports.article = require('./article.controller');
 exports.category = require('./category.controller');
 exports.announcement = require('./announcement.controller');
