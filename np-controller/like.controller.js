e9fe0c15bcf7901cd6dcb21136443fe11978f75a
surmon-china <surmon@foxmail.com>
2017-03-05 03:32:23 +0800

    building

--- np-controller/like.controller.js
+++ np-controller/like.controller.js
@@ -5,9 +5,32 @@
 */
 
 const { handleError, handleSuccess } = require('np-utils/np-handle');
+
+const Article = require('np-model/article.model');
+const Comment = require('np-model/comment.model');
+const Option = require('np-model/option.model');
+
 const config = require('np-config');
 
-module.exports = (req, res) => {
-	console.log(req.body)
-	res.jsonp({ code: 1, message: '点赞成功', result: {}});
+module.exports = ({ body: { id, type }}, res) => {
+
+	// 验证
+	if (![1, 2].includes(type)) {
+		res.jsonp({ code: 0, message: '点赞失败，没有原因' });
+	}
+
+	// like
+	(Object.is(type, 1) ? Comment : (Object.is(id, 0) ? Option : Article))
+	.findOne((() => (Object.is(id, 0)) ? null : { id })())
+	.then(result => {
+		if (Object.is(type, 1)) {
+			result.likes ++;
+		} else {
+			result.meta.likes ++;
+		}
+		result.save();
+		handleSuccess({ res, result, message: '点赞成功' });
+	}).catch(err => {
+		handleError({ res, err, message: '公告发布失败' });
+	})
 };
