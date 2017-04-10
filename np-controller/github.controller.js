8bc2c1b090f53252870ebf4062875af2884d8007
surmon-china <surmon.me@gmail.com>
2017-04-10 23:21:26 +0800

    update

--- np-controller/github.controller.js
+++ np-controller/github.controller.js
@@ -20,8 +20,10 @@ const getGithubRepositories = () => {
     if(!err && response.statusCode == 200) {
       currentRepositories = JSON.parse(body);
     } else {
-      console.warn('项目列表获取失败');
+      console.warn('项目列表获取失败', 'err:', err, 'body:', body);
     }
+    // 无论成功失败都定时更新，10分钟一次
+    setTimeout(getGithubRepositories, 1000 * 60 * 10);
   })
 }
 
@@ -30,7 +32,6 @@ getGithubRepositories();
 // 获取项目列表
 githubCtrl.GET = (req, res) => {
   handleSuccess({ res, result: currentRepositories, message: '项目列表获取成功' });
-  getGithubRepositories();
 };
 
 // export
