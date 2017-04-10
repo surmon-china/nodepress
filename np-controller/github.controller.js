a77a76952cb397c91c4834f5c09d9ebdf71d2c27
surmon-china <surmon.me@gmail.com>
2017-04-10 15:07:30 +0800

    optimize github api cache

--- np-controller/github.controller.js
+++ np-controller/github.controller.js
@@ -8,18 +8,29 @@ const request = require('request');
 const { handleRequest, handleError, handleSuccess } = require('np-utils/np-handle');
 const githubCtrl = {};
 
-// 获取项目列表
-githubCtrl.GET = (req, res) => {
+// 保存内存中代理redis
+let currentRepositories = [];
+
+// 获取远程项目列表
+const getGithubRepositories = () => {
   request({
     url: 'https://api.github.com/users/surmon-china/repos',
     headers: { 'User-Agent': 'request' }
   }, (err, response, body) => {
     if(!err && response.statusCode == 200) {
-      handleSuccess({ res, result: JSON.parse(body), message: '项目列表获取成功' });
+      currentRepositories = JSON.parse(body);
     } else {
-      handleError({ res, err, message: '项目列表获取失败' });
+      console.warn('项目列表获取失败');
     }
   })
+}
+
+getGithubRepositories();
+
+// 获取项目列表
+githubCtrl.GET = (req, res) => {
+  handleSuccess({ res, result: currentRepositories, message: '项目列表获取成功' });
+  getGithubRepositories();
 };
 
 // export
