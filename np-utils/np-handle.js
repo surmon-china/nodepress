34ec42318ce26f56ed943a1e08b7c1d9d73eca87
surmon <mcc@meitu.com>
2018-01-10 22:31:32 +0800

    add http status

--- np-utils/np-handle.js
+++ np-utils/np-handle.js
@@ -7,8 +7,12 @@ exports.handleRequest = ({ req, res, controller }) => {
 	support || res.status(405).jsonp({ code: 0, message: '不支持该请求类型！' });
 };
 
-exports.handleError = ({ res, message = '请求失败', err = null }) => {
-	res.jsonp({ code: 0, message, debug: err });
+exports.handleError = ({ res, message = '请求失败', err = null, code }) => {
+	if (code) {
+		res.status(code).jsonp({ code: 0, message, debug: err });
+	} else {
+		res.jsonp({ code: 0, message, debug: err });
+	}
 };
 
 exports.handleSuccess = ({ res, message = '请求成功', result = null }) => {
