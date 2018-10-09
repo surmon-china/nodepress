0b536624941112615069ad74ef155e1c18cb9fd9
Surmon <surmon@qq.com>
2018-10-10 02:20:15 +0800

    update

--- np-controller/github.controller.js
+++ np-controller/github.controller.js
@@ -4,12 +4,11 @@
 *
 */
 
-const request = require('request');
+const request = require('request')
+const redis = require('np-core/np-redis')
+const { handleRequest, handleError, handleSuccess } = require('np-utils/np-handle')
 
-const redis = require('np-core/np-redis');
-const { handleRequest, handleError, handleSuccess } = require('np-utils/np-handle');
-
-const githubCtrl = {};
+const githubCtrl = {}
 
 // 获取远程项目列表
 const getGithubRepositories = () => {
@@ -18,23 +17,37 @@ const getGithubRepositories = () => {
 		headers: { 'User-Agent': 'request' }
 	}, (err, response, body) => {
 		if(!err && response.statusCode == 200) {
-			redis.set('github-projects', body);
+			const peojects = JSON.parse(body).map(rep => {
+				return {
+					html_url: rep.html_url,
+					name: rep.name || ' ',
+					fork: rep.fork,
+					forks: rep.forks,
+					forks_count: rep.forks_count,
+					description: rep.description || ' ',
+					open_issues_count: rep.open_issues_count,
+					stargazers_count: rep.stargazers_count,
+					created_at: rep.created_at,
+					language: rep.language
+				}
+			})
+			redis.set('github-projects', peojects)
 		} else {
-			console.warn('项目列表获取失败', 'err:', err, 'body:', body);
+			console.warn('项目列表获取失败', 'err:', err, 'body:', body)
 		}
 		// 无论成功失败都定时更新，10分钟一次
-		setTimeout(getGithubRepositories, 1000 * 60 * 10);
+		setTimeout(getGithubRepositories, 1000 * 60 * 10)
 	})
-};
+}
 
-getGithubRepositories();
+getGithubRepositories()
 
 // 获取项目列表
 githubCtrl.GET = (req, res) => {
 	redis.get('github-projects', (err, github_projects) => {
-		handleSuccess({ res, result: github_projects, message: '项目列表获取成功' });
+		handleSuccess({ res, result: github_projects, message: '项目列表获取成功' })
 	})
-};
+}
 
 // export
-module.exports = (req, res) => { handleRequest({ req, res, controller: githubCtrl })};
+module.exports = (req, res) => { handleRequest({ req, res, controller: githubCtrl })}
--- np-controller/music.controller.js
+++ np-controller/music.controller.js
@@ -4,75 +4,79 @@
 *
 */
 
-const NeteseMusic = require('simple-netease-cloud-music');
+const NeteseMusic = require('simple-netease-cloud-music')
 
-const { handleRequest, handleError, handleSuccess } = require('np-utils/np-handle');
+const { handleRequest, handleError, handleSuccess } = require('np-utils/np-handle')
 
-const neteseMusic = new NeteseMusic();
-const musicCtrl = { lrc: {}, list: {}, song: {}, url: {}, pic: {} };
+const neteseMusic = new NeteseMusic()
+const musicCtrl = { lrc: {}, list: {}, song: {}, url: {}, pic: {} }
 
 // 获取某歌单列表
 musicCtrl.list.GET = (req, res) => {
-	const play_list_id = req.params.play_list_id;
-	if (!play_list_id || Object.is(Number(play_list_id), NaN)) {
-		handleError({ res, message: '参数无效' });
-		return false;
+	const limit_num = req.query.limit || 30
+	const play_list_id = req.params.play_list_id
+	if (!play_list_id || isNaN(Number(play_list_id))) {
+		handleError({ res, message: '参数无效' })
+		return false
 	}
 	neteseMusic._playlist(play_list_id).then(({ playlist }) => {
-		handleSuccess({ res, result: playlist, message: '歌单列表获取成功' });
-	});
-};
+		// 歌单需要进行一些截取
+		Reflect.deleteProperty(playlist, 'trackIds')
+		playlist.tracks = playlist.tracks.slice(0, limit_num)
+		handleSuccess({ res, result: playlist, message: '歌单列表获取成功' })
+	})
+}
 
 // 获取歌曲详情
 musicCtrl.song.GET = (req, res) => {
-	const song_id = req.params.song_id;
+	const song_id = req.params.song_id
 	if (!song_id || Object.is(Number(song_id), NaN)) {
-		handleError({ res, message: '参数无效' });
-		return false;
+		handleError({ res, message: '参数无效' })
+		return false
 	}
 	neteseMusic.song(song_id).then(result => {
-		handleSuccess({ res, result, message: '歌曲详情获取成功' });
-	});
-};
+		handleSuccess({ res, result, message: '歌曲详情获取成功' })
+	})
+}
 
 // 获取歌曲地址
 musicCtrl.url.GET = (req, res) => {
-	const song_id = req.params.song_id;
+	const song_id = req.params.song_id
 	if (!song_id || Object.is(Number(song_id), NaN)) {
-		handleError({ res, message: '参数无效' });
-		return false;
+		handleError({ res, message: '参数无效' })
+		return false
 	}
 	neteseMusic.url(song_id, 128).then(result => {
-		handleSuccess({ res, result, message: '歌曲地址获取成功' });
-	});
-};
+		handleSuccess({ res, result, message: '歌曲地址获取成功' })
+	})
+}
 
 // 获取歌词
 musicCtrl.lrc.GET = (req, res) => {
-	const song_id = req.params.song_id;
+	const song_id = req.params.song_id
 	if (!song_id || Object.is(Number(song_id), NaN)) {
-		handleError({ res, message: '参数无效' });
-		return false;
+		handleError({ res, message: '参数无效' })
+		return false
 	}
 	neteseMusic.lyric(song_id).then(result => {
-		handleSuccess({ res, result, message: '歌词获取成功' });
-	});
-};
+		handleSuccess({ res, result, message: '歌词获取成功' })
+	})
+}
 
 // 获取图片封面
 musicCtrl.pic.GET = (req, res) => {
-	const pic_id = req.params.pic_id;
+	const pic_id = req.params.pic_id
 	if (!pic_id || Object.is(Number(pic_id), NaN)) {
-		handleError({ res, message: '参数无效' });
-		return false;
+		handleError({ res, message: '参数无效' })
+		return false
 	}
 	neteseMusic.picture(pic_id, 700).then(result => {
-		handleSuccess({ res, result, message: '封面获取成功' });
-	});
-};
+		handleSuccess({ res, result, message: '封面获取成功' })
+	})
+}
 
-exports.pic = (req, res) => { handleRequest({ req, res, controller: musicCtrl.pic })};
-exports.url = (req, res) => { handleRequest({ req, res, controller: musicCtrl.url })};
-exports.lrc = (req, res) => { handleRequest({ req, res, controller: musicCtrl.lrc })};
-exports.list = (req, res) => { handleRequest({ req, res, controller: musicCtrl.list })};
-exports.song = (req, res) => { handleRequest({ req, res, controller: musicCtrl.song })};
+exports.pic = (req, res) => { handleRequest({ req, res, controller: musicCtrl.pic })}
+exports.url = (req, res) => { handleRequest({ req, res, controller: musicCtrl.url })}
+exports.lrc = (req, res) => { handleRequest({ req, res, controller: musicCtrl.lrc })}
+exports.list = (req, res) => { handleRequest({ req, res, controller: musicCtrl.list })}
+exports.song = (req, res) => { handleRequest({ req, res, controller: musicCtrl.song })}
