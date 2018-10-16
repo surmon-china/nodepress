8c3e6175e795d6155ab85370166cfa140c202b46
surmon-china <surmon@foxmail.com>
2018-10-17 01:08:31 +0800

    update redis cache config and doDeploy

--- np-controller/github.controller.js
+++ np-controller/github.controller.js
@@ -10,6 +10,27 @@ const consola = require('consola')
 const CONFIG = require('app.config')
 const redis = require('np-core/np-redis')
 const { handleSuccess } = require('np-core/np-processor')
+const { REDIS_CACHE_FIELDS } = require('np-core/np-constants')
+
+// 存储数据
+const saveGithubData = data => {
+	const peojects = data.map(rep => {
+		return {
+			html_url: rep.html_url,
+			name: rep.name || ' ',
+			fork: rep.fork,
+			forks: rep.forks,
+			forks_count: rep.forks_count,
+			description: rep.description || ' ',
+			open_issues_count: rep.open_issues_count,
+			stargazers_count: rep.stargazers_count,
+			created_at: rep.created_at,
+			language: rep.language
+		}
+	})
+	// consola.success('项目列表获取成功', peojects)
+	redis.set(REDIS_CACHE_FIELDS.github, peojects)
+}
 
 // 获取远程项目列表
 ;((function getGithubRepositories() {
@@ -18,24 +39,13 @@ const { handleSuccess } = require('np-core/np-processor')
 		headers: { 'User-Agent': 'request' }
 	}, (err, response, body) => {
 		if (!err && response.statusCode == 200) {
-			const peojects = JSON.parse(body).map(rep => {
-				return {
-					html_url: rep.html_url,
-					name: rep.name || ' ',
-					fork: rep.fork,
-					forks: rep.forks,
-					forks_count: rep.forks_count,
-					description: rep.description || ' ',
-					open_issues_count: rep.open_issues_count,
-					stargazers_count: rep.stargazers_count,
-					created_at: rep.created_at,
-					language: rep.language
-				}
-			})
-			// consola.success('项目列表获取成功', peojects)
-			redis.set('github-projects', peojects)
+			try {
+				saveGithubData(JSON.parse(body))
+			} catch (error) {
+				consola.warn('github 控制器解析为 JSON 失败', new Date(), body)
+			}
 		} else {
-			consola.warn('项目列表获取失败', 'err:', err, 'body:', body)
+			consola.warn('项目列表获取失败', new Date(), 'err:', err, 'body:', body)
 		}
 		// 无论成功失败都定时更新，10 分钟一次
 		setTimeout(getGithubRepositories, 1000 * 60 * 10)
@@ -44,7 +54,7 @@ const { handleSuccess } = require('np-core/np-processor')
 
 // 获取内存中项目列表数据
 module.exports = (req, res) => {
-	redis.get('github-projects', (err, result) => {
+	redis.get(REDIS_CACHE_FIELDS.github, (err, result) => {
 		handleSuccess({ res, result, message: '项目列表获取成功' })
 	})
 }
--- np-controller/wallpaper.controller.js
+++ np-controller/wallpaper.controller.js
@@ -5,37 +5,95 @@
  * @author Surmon <https://github.com/surmon-china>
  */
 
+const consola = require('consola')
+const redis = require('np-core/np-redis')
 const WonderfulBingWallpaper = require('wonderful-bing-wallpaper')
+const { REDIS_CACHE_FIELDS } = require('np-core/np-constants')
 const {
+	handleError,
 	handleSuccess,
-	humanizedHandleSuccess,
-	humanizedHandleError,
 	buildController,
 	initController
 } = require('np-core/np-processor')
 
-const wbw = new WonderfulBingWallpaper({ local: 'zh-CN', host: 'cn.bing.com' })
 const WallpaperCtrl = initController(['list', 'story'])
+const wbw = new WonderfulBingWallpaper({ local: 'zh-CN', host: 'cn.bing.com' })
+
+// 默认请求参数
+const WALLPAPER_PARAMS = { size: 8 }
+
+// 几小时
+const hours = hours => 1000 * 60 * 60 * hours
+
+// 几小时更新一次数据
+const intervalGetPapers = h => setTimeout(getWallpapers, hours(h))
 
 // 获取今日壁纸
+const getWallpapers = ({ res = null, params = WALLPAPER_PARAMS, isRedis = true }) => {
+  wbw.getWallpapers(params)
+    .then(wallpaperJSON => {
+			try {
+				const result = wbw.humanizeWallpapers(wallpaperJSON)
+				isRedis
+					? redis.set(REDIS_CACHE_FIELDS.wallpapers, result) && intervalGetPapers(1)
+					: handleSuccess({ res, result, message: '今日壁纸获取成功' })
+			} catch (err) {
+				isRedis
+					? intervalGetPapers(0.5)
+					: handleError({ res, err, message: '今日壁纸获取失败' })
+				consola.warn('wallpaper 控制器解析 JSON 失败', new Date(), wallpaperJSON)
+			}
+    })
+    .catch(err => {
+			isRedis
+				? intervalGetPapers(0.5)
+				: handleError({ res, err, message: '今日壁纸获取失败' })
+			consola.warn('getWallpapers 获取多个壁纸出现了问题', new Date(), err)
+		})
+}
+
+// 初始化自动请求
+getWallpapers({})
+
+// 请求响应
 WallpaperCtrl.list.GET = (req, res) => {
 	const { size, day } = req.query
-	const params = {}
-	day && (params.day = day)
-	size && (params.size = size)
-	wbw.getWallpapers(params)
-		.then(wallpaperJSON => {
-			const result = wbw.humanizeWallpapers(wallpaperJSON)
+
+	// 如果请求的是默认数据则直接返回内存中的
+	const defaultDay = day === undefined || day == 0
+	const defaultSize = size === undefined || size == 8
+	if (defaultDay && defaultSize) {
+		redis.get(REDIS_CACHE_FIELDS.wallpapers, (err, result) => {
 			handleSuccess({ res, result, message: '今日壁纸获取成功' })
 		})
-		.catch(humanizedHandleError(res, '今日壁纸获取失败'))
+
+	// 否则重新走正常流程
+	} else {
+		const params = {}
+		day && (params.day = day)
+		size && (params.size = size)
+		getWallpapers({ res, params, isRedis: false })
+	}
 }
+
+// 获取今日壁纸故事并缓存
+;((function getTodayStory() {
+	wbw.getTodayWallpaperStory()
+		.then(story => {
+			redis.set(REDIS_CACHE_FIELDS.wallpaperStory, story)
+			setTimeout(getTodayStory, hours(1))
+		})
+		.catch(err => {
+			consola.warn('今日壁纸故事远程请求失败：', new Date(), err)
+			setTimeout(getTodayStory, hours(0.5))
+		})
+})())
 
 // 获取今日壁纸故事
 WallpaperCtrl.story.GET = (req, res) => {
-	wbw.getTodayWallpaperStory()
-		.then(humanizedHandleSuccess(res, '今日壁纸故事获取成功'))
-		.catch(humanizedHandleError(res, '今日壁纸故事获取失败'))
+	redis.get(REDIS_CACHE_FIELDS.wallpaperStory, (err, result) => {
+		handleSuccess({ res, result, message: '今日壁纸故事获取成功' })
+	})
 }
 
 exports.list = buildController(WallpaperCtrl.list)
