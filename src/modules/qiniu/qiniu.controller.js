c451ab3e27a66648efd3a6b421d32408ceb14f5d
surmon-china <surmon@foxmail.com>
2018-12-08 04:48:41 +0800

    初始化项目结构

---
+++ src/modules/qiniu/qiniu.controller.js
@@ -0,0 +1,21 @@
+/**
+ * QiniuCtrl module.
+ * @file 七牛控制器模块
+ * @module controller/qiniu
+ * @author Surmon <https://github.com/surmon-china>
+ */
+
+const qiniu = require('qn')
+const CONFIG = require('app.config')
+const { buildController, initController, handleSuccess } = require('np-core/np-processor')
+
+const client = qiniu.create(CONFIG.QINIU)
+const QiniuCtrl = initController()
+
+// 获取配置列表
+QiniuCtrl.GET = (req, res) => {
+  const result = { upToken: client.uploadToken() }
+  handleSuccess({ res, result, message: 'upToken 获取成功' })
+}
+
+module.exports = buildController(QiniuCtrl)
