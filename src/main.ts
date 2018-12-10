7d65e2a79ed61126313d0ae50a5791f01d6a1bbd
surmon-china <surmon@foxmail.com>
2018-12-11 00:36:53 +0800

    优化错误处理模块

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,17 @@
 
 import * as helmet from 'helmet';
 import * as bodyParser from 'body-parser';
+import * as appConfig from '@app/app.config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
-import * as appConfig from '@app/app.config';
+import { ErrorFilter } from '@app/filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.use(bodyParser.json({ limit: '1mb' }));
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.useGlobalFilters(new ErrorFilter());
   return await app.listen(appConfig.APP.PORT);
 }
 
