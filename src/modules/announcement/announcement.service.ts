1b75953a97b448e3cf20805a6c74f16adc6cb64c
surmon-china <surmon@foxmail.com>
2018-12-17 04:03:12 +0800

    完美 HACK 接口

--- src/modules/announcement/announcement.service.ts
+++ src/modules/announcement/announcement.service.ts
@@ -1,29 +1,19 @@
 
-import { Document, Model, PaginateOptions, PaginateResult } from 'mongoose';
-import { Injectable, Inject } from '@nestjs/common';
+import { PaginateResult } from 'mongoose';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
-import { ModelType } from 'typegoose';
+import { TMongooseModel } from '@app/interfaces/mongoose.interface';
 import { Announcement } from './announcement.modal';
-import { CreateAnnouncementDto } from './bak/announcement.dto';
-
-interface PaginateModel<T extends Document> extends Model<T> {
-  paginate(
-    query?: object,
-    options?: PaginateOptions,
-  );
-}
-
-type test<T> = Model<T & Document> & T;
 
 @Injectable()
 export class AnnouncementService {
-  constructor(@InjectModel(Announcement) private readonly announcementModel: test<Announcement>) {}
+  constructor(@InjectModel(Announcement) private readonly announcementModel: TMongooseModel<Announcement>) {}
 
-  async findAll(query, options): Promise<Announcement[]> {
-    return await (this.announcementModel as any).paginate(query, options);
+  async findAll(query, options): Promise<PaginateResult<Announcement>> {
+    return await this.announcementModel.paginate(query, options);
   }
 
-  async create(createAnnouncementDto: CreateAnnouncementDto): Promise<Announcement> {
+  async create(createAnnouncementDto: Announcement): Promise<Announcement> {
     const createdAnnouncement = new this.announcementModel(createAnnouncementDto);
     return await createdAnnouncement.save();
   }
