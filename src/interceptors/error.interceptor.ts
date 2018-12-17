6655156c7fbad7328076e23c9d5f1863fd215b2f
surmon-china <surmon.me@gmail.com>
2018-12-18 02:39:43 +0800

    完善权限控制

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -3,7 +3,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Reflector } from '@nestjs/core';
 import { TMessage } from '@app/interfaces/http.interface';
-import buildCustomError from '@app/errors/custom.error';
+import { CustomError } from '@app/errors/custom.error';
 import * as META from '@app/constants/meta.constant';
 import * as TEXT from '@app/constants/text.constant';
 
@@ -15,7 +15,7 @@ export class ErrorInterceptor implements NestInterceptor {
     const statusCode = this.reflector.get<HttpStatus>(META.HTTP_ERROR_CODE, target);
     const message = this.reflector.get<TMessage>(META.HTTP_ERROR_MESSAGE, target) || TEXT.HTTP_DEFAULT_ERROR_TEXT;
     return call$.pipe(
-      catchError(error => throwError(buildCustomError({ message, error }, statusCode))),
+      catchError(error => throwError(new CustomError({ message, error }, statusCode))),
     );
   }
 }
