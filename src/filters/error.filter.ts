feaf30ea6e6edf16643ab13a4bd2c86762bd81b4
surmon-china <surmon@foxmail.com>
2018-12-23 05:54:25 +0800

    完成公告模块开发

--- src/filters/error.filter.ts
+++ src/filters/error.filter.ts
@@ -4,22 +4,26 @@ import { isDevMode } from '@app/app.environment';
 import { EStatus, THttpErrorResponse, TExceptionOption, TMessage } from '@app/interfaces/http.interface';
 import { ExceptionFilter, Catch, HttpException, ArgumentsHost, HttpStatus } from '@nestjs/common';
 
-// new HttpException({ message: '错误信息', error: '错误信息来源' }, status)
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+    // const request = host.switchToHttp().getRequest();
     const response = host.switchToHttp().getResponse();
     const status = exception.getStatus() || HttpStatus.INTERNAL_SERVER_ERROR;
     const errorOption: TExceptionOption = exception.message as TExceptionOption;
     const isString = (value): value is TMessage => lodash.isString(value);
     const errMessage = isString(errorOption) ? errorOption : errorOption.message;
     const errorInfo = isString(errorOption) ? null : errorOption.error;
+    const parentErrorInfo = errorInfo ? String(errorInfo) : null;
+    const isChildrenError = errorInfo.status && errorInfo.message;
+    const resultError = isChildrenError ? errorInfo.message : parentErrorInfo;
+    const resultStatus = isChildrenError ? errorInfo.status : status;
     const data: THttpErrorResponse = {
       status: EStatus.Error,
       message: errMessage,
-      error: errorInfo ? String(errorInfo) : null,
+      error: resultError,
       debug: isDevMode ? exception.stack : null,
     };
-    return response.status(status).jsonp(data);
+    return response.status(resultStatus).jsonp(data);
   }
 }
