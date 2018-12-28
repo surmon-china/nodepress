f1721ae3c336d6fc118ca1c47005e496181639cf
surmon-china <surmon@foxmail.com>
2018-12-28 18:24:11 +0800

    晚上辅助库

---
+++ src/processors/helper/baidu-seo.service.ts
@@ -0,0 +1,55 @@
+/**
+ * Extended Giithub service.
+ * @file Extended Giithub 模块服务
+ * @module modules/extended/github.service
+ * @author Surmon <https://github.com/surmon-china>
+ */
+
+import * as APP_CONFIG from '@app/app.config';
+import { Injectable, HttpService } from '@nestjs/common';
+
+export type TUrl = string;
+
+export interface IBaiduSeoRequestOption {
+  urlKey: string;
+  urls: TUrl[];
+  action: string;
+}
+
+@Injectable()
+export class BaiduSeoService {
+  constructor(private readonly httpService: HttpService) {}
+
+  // POST 服务
+  private baiduRequest(option: IBaiduSeoRequestOption): Promise<any> {
+    const { urlKey, urls, action } = option;
+    return this.httpService.axiosRef.request({
+      method: 'post',
+      data: urls.join('\n'),
+      headers: { 'Content-Type': 'text/plain' },
+      url: `http://data.zz.baidu.com/${urlKey}?site=${APP_CONFIG.BAIDU.site}&token=${APP_CONFIG.BAIDU.token}`,
+    }).then(response => {
+      console.info(`${action} 操作：`, urls, response);
+      return response;
+    }).catch(error => {
+      const message = (error.response && error.response.data) || error;
+      console.warn(`${action} 操作失败：`, message);
+      return Promise.reject(message);
+    });
+  }
+
+  // 提交记录
+  push(url: TUrl) {
+    this.baiduRequest({ urls: [url], urlKey: 'urls', action: '百度推送' });
+  }
+
+  // 更新记录
+  update(url: TUrl) {
+    this.baiduRequest({ urls: [url], urlKey: 'update', action: '百度更新' });
+  }
+
+  // 删除记录
+  delete(url: TUrl) {
+    this.baiduRequest({ urls: [url], urlKey: 'del', action: '百度删除' });
+  }
+}
--- src/processors/helper/helper.module.ts
+++ src/processors/helper/helper.module.ts
@@ -5,7 +5,7 @@
  * @author Surmon <https://github.com/surmon-china>
  */
 
-import { Module, HttpModule } from '@nestjs/common';
+import { Module, Global, HttpModule } from '@nestjs/common';
 import { AkismetService } from './akismet.service';
 import { BaiduSeoService } from './baidu-seo.service';
 import { EmailService } from './email.service';
@@ -13,6 +13,8 @@ import { IpService } from './ip.service';
 
 const services = [AkismetService, BaiduSeoService, EmailService, IpService];
 
+// todo -> 测试全局和局部引入是否单例
+@Global()
 @Module({
   imports: [HttpModule],
   providers: services,
