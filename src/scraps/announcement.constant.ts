1cc7e9f8d76c88c4c896a77f43f156d053fbbd55
surmon-china <surmon.me@gmail.com>
2019-01-07 03:51:53 +0800

    rebuild

--- src/scraps/announcement.constant.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Announcement constant.
- * @file 公告模块常量
- * @module modules/announcement/constant
- * @author Surmon <https://github.com/surmon-china>
- */
-
-export const NAME = 'Announcement';
-export const MODEL_TOKEN = NAME + 'ModelToken';
