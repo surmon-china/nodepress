a951019a539177a8e1ed640b7d248ce8db870fc0
surmon-china <surmon.me@gmail.com>
2019-10-09 01:00:36 +0800

    Update deploy scripts

--- .deploy/main.sh
+++ .deploy/main.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-SHELL_PATH=$(dirname $0)
+WEB_PATH=$(dirname $0)
+# WEB_USER='root'
+# WEB_USERGROUP='root'
 
 echo "[deploy] Start deployment..."
-sh ${SHELL_PATH}/rebuild.sh
-sh ${SHELL_PATH}/restart.sh
+ 
+echo "[deploy] Fetch and rebuilding..."
+cd $WEB_PATH
+cd ..
+echo "[deploy] path:" $(pwd)
+echo "[deploy] pulling source code..."
+git fetch --all && git reset --hard origin/master && git pull
+git checkout master
+# echo "changing permissions..."
+# chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+# chmod -R 777 $WEB_PATH
+# sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+echo "[deploy] Install dependencies..."
+npm ci
+
+echo "[deploy] Stop service..."
+pm2 stop nodepress
+
+echo "[deploy] Remove old dist..."
+rm -r ./dist/*
+
+echo "[deploy] Building..."
+npm run build
+
+echo "[deploy] Restarting..."
+pm2 restart nodepress
+
 echo "[deploy] Finished."
