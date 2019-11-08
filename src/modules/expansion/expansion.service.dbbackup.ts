1f3243c2fcef1cc63aac8e3d9cb1a656d35ef75b
surmon-china <surmon@foxmail.com>
2019-11-08 23:58:27 +0800

    Upgrade to v3.2.4

--- src/modules/expansion/expansion.service.dbbackup.ts
+++ src/modules/expansion/expansion.service.dbbackup.ts
@@ -7,44 +7,25 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
-import * as qiniu from 'qiniu';
 import * as shell from 'shelljs';
 import * as moment from 'moment';
 import * as schedule from 'node-schedule';
 import * as APP_CONFIG from '@app/app.config';
 import { Injectable } from '@nestjs/common';
+import { CloudStorageService } from '@app/processors/helper/helper.service.cs';
 
 // Configs
 const UPFAILE_TIMEOUT = 1000 * 60 * 5;
 const UPLOAD_INTERVAL = '0 0 3 * * *';
 
 const BACKUP_FILE_EXT = '.tar.gz';
-const BACK_UP_SHELL_PATH = path.normalize(APP_CONFIG.DB_BACKUP.backupShellPath);
-const BACK_UP_DATA_PATH = path.resolve(APP_CONFIG.DB_BACKUP.backupFilePath, `nodepress${BACKUP_FILE_EXT}`);
-
-// Qiniu sdk
-const mac = new qiniu.auth.digest.Mac(
-  APP_CONFIG.DB_BACKUP.accessKey,
-  APP_CONFIG.DB_BACKUP.secretKey,
-);
-
-const putPolicy = new qiniu.rs.PutPolicy({
-  scope: APP_CONFIG.DB_BACKUP.bucket,
-});
-
-const qiniuConfig = new qiniu.conf.Config({
-  zone: qiniu.zone.Zone_z0,
-  useHttpsDomain: false,
-  useCdnDomain: false,
-});
-
-const formUploader = new qiniu.form_up.FormUploader(qiniuConfig);
-const putExtra = new qiniu.form_up.PutExtra();
+const BACKUP_SHELL_PATH = path.normalize(APP_CONFIG.DB_BACKUP.backupShellPath);
+const BACKUP_DATA_PATH = path.resolve(APP_CONFIG.DB_BACKUP.backupFilePath, `nodepress${BACKUP_FILE_EXT}`);
 
 @Injectable()
 export class DBBackupcService {
 
-  constructor() {
+  constructor(private readonly cloudStorageService: CloudStorageService) {
     console.log('DB Backup 开始执行定时数据备份任务！');
     schedule.scheduleJob(UPLOAD_INTERVAL, () => {
       this.backup().catch(() => {
@@ -56,35 +37,32 @@ export class DBBackupcService {
   // 打包备份并上传
   public backup(): Promise<void> {
     return new Promise((resolve, reject) => {
-      if (!fs.existsSync(BACK_UP_SHELL_PATH)) {
+      if (!fs.existsSync(BACKUP_SHELL_PATH)) {
         return reject('DB Backup shell 脚本不存在');
       }
 
-      shell.exec(`sh ${BACK_UP_SHELL_PATH}`, (code, stdout, stderr) => {
+      shell.exec(`sh ${BACKUP_SHELL_PATH}`, (code, stdout, stderr) => {
 
         const fileDate = moment(new Date()).format('YYYY-MM-DD-HH:mm');
         const fileName = `nodepress-db-backup-${fileDate}${BACKUP_FILE_EXT}`;
         console.info('DB Backup shell 执行完成！', code);
         console.info('DB Backup 上传文件: ' + fileName);
-        console.info('DB Backup 文件源位置: ' + BACK_UP_DATA_PATH);
+        console.info('DB Backup 文件源位置: ' + BACKUP_DATA_PATH);
 
-        // get token
-        const uptoken = putPolicy.uploadToken(mac);
-
-        // upload file
-        formUploader.putFile(uptoken, fileName, BACK_UP_DATA_PATH, putExtra, (respErr, respBody, respInfo) => {
-          if (respErr) {
-            console.warn('DB Backup 备份数据上传失败!', respErr);
-            return reject(String(respErr));
-          }
-
-          if (respInfo.statusCode === 200) {
-            console.info('DB Backup 备份数据上传成功!', respBody.key);
-            return resolve();
-          }
-
-          console.warn('DB Backup 备份数据上传状态响应异常!', respInfo.statusCode, respBody);
-          reject(JSON.stringify(respBody));
+        // 上传文件
+        this.cloudStorageService.updateFile(
+          fileName,
+          BACKUP_DATA_PATH,
+          APP_CONFIG.DB_BACKUP.bucket,
+          APP_CONFIG.DB_BACKUP.region,
+        )
+        .then(result => {
+          console.info('DB Backup 备份数据上传成功!', result);
+          return resolve();
+        })
+        .catch(error => {
+          console.warn('DB Backup 备份数据上传状态响应异常!', error);
+          reject(JSON.stringify(error.message));
         });
       });
     });
