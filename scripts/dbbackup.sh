3b2549e35f0d2436baeaa408d642196654cb5dc2
surmon-china <surmon.me@gmail.com>
2021-06-10 23:35:16 +0800

    chore: NIP

--- scripts/dbbackup.sh
+++ scripts/dbbackup.sh
@@ -4,8 +4,8 @@ WEB_PATH=$(dirname $0)
 
 # MARK: 需保证脚本的独立性，不可避免地需要一些部分与业务代码耦合，此处将 <BACK_PATH> 与 <BACK_FILE> 约定为耦合部分
 # BACK_PATH 默认值为 '../dbbackup'
-BACK_PATH='dbbackup'
 # BACK_FILE 默认值为 'nodepress.tar.gz'
+BACK_PATH='dbbackup'
 BACK_FILE='nodepress.tar.gz'
 
 cd $WEB_PATH
