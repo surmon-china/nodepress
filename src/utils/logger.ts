cc3e933020a5b5ad53b2dce79da13e2b9db4da58
surmon-china <surmon.me@gmail.com>
2021-12-07 17:09:08 +0800

    feat: fix logger type

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,50 +1,53 @@
 /**
- * @file App global logger
+ * @file App logger
  * @module utils/logger
  * @author Surmon <https://github.com/surmon-china>
  */
 
 import chalk from 'chalk'
 
-enum LoggerFn {
+enum LoggerLevel {
   Debug = 'debug',
   Info = 'info',
   Warn = 'warn',
   Error = 'error',
 }
 
-const createLogger = (namespace = 'NP') => {
-  const renderTime = () => {
-    const now = new Date()
-    return `[${now.toLocaleDateString()} ${now.toLocaleTimeString()}]`
-  }
-  const renderModule = (message: string) => {
-    if (typeof message === 'string' && message.startsWith('[') && message.endsWith(']')) {
-      return chalk.green.underline(message.substr(1, message.length - 2))
-    } else {
-      return message
-    }
-  }
-  const renderMessage = (color: any, messages: any[]) => {
-    return messages.map((m) => (typeof m === 'string' ? color(m) : m))
-  }
-  const renderLog = (method: LoggerFn, levelLabel: any, messageColor: any) => {
-    return (message: string, ...args: any) => {
-      return console[method](
-        chalk.greenBright(`[${namespace}]`),
-        renderTime(),
-        levelLabel,
-        renderModule(message),
-        ...renderMessage(messageColor, args)
-      )
-    }
-  }
+const renderTime = () => {
+  const now = new Date()
+  return `[${now.toLocaleDateString()} ${now.toLocaleTimeString()}]`
+}
 
+const renderModule = (message: string) => {
+  if (typeof message === 'string' && message.startsWith('[') && message.endsWith(']')) {
+    return chalk.green.underline(message.substr(1, message.length - 2))
+  } else {
+    return message
+  }
+}
+
+const renderMessage = (color: chalk.Chalk, messages: any[]) => {
+  return messages.map((m) => (typeof m === 'string' ? color(m) : m))
+}
+
+const renderLog = (method: LoggerLevel, levelLabel: string, messageColor: chalk.Chalk) => {
+  return (message: string, ...args: any) => {
+    return console[method](
+      chalk.greenBright(`[NP]`),
+      renderTime(),
+      levelLabel,
+      renderModule(message),
+      ...renderMessage(messageColor, args)
+    )
+  }
+}
+
+const createLogger = () => {
   return {
-    debug: renderLog(LoggerFn.Debug, chalk.cyan('[DEBUG]'), chalk.cyanBright),
-    info: renderLog(LoggerFn.Info, chalk.blue('[_INFO]'), chalk.greenBright),
-    warn: renderLog(LoggerFn.Warn, chalk.yellow('[_WARN]'), chalk.yellowBright),
-    error: renderLog(LoggerFn.Error, chalk.red('[ERROR]'), chalk.redBright),
+    debug: renderLog(LoggerLevel.Debug, chalk.cyan('[DEBUG]'), chalk.cyanBright),
+    info: renderLog(LoggerLevel.Info, chalk.blue('[_INFO]'), chalk.greenBright),
+    warn: renderLog(LoggerLevel.Warn, chalk.yellow('[_WARN]'), chalk.yellowBright),
+    error: renderLog(LoggerLevel.Error, chalk.red('[ERROR]'), chalk.redBright),
   }
 }
 
