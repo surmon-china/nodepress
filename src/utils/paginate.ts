1e52466a9e888dea89954da6329624b8dfa56a1f
Surmon <surmon.me@gmail.com>
2026-01-04 00:23:04 +0800

    feat: redesign the paginate utility to provide stricter type constraints.

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -5,7 +5,7 @@
  */
 
 import _merge from 'lodash/merge'
-import type { Model, Document, Schema, FilterQuery, QueryOptions } from 'mongoose'
+import type { Model, Schema, QueryFilter, QueryOptions, HydratedDocument } from 'mongoose'
 
 export interface PaginateResult<T> {
   documents: Array<T>
@@ -15,7 +15,6 @@ export interface PaginateResult<T> {
   totalPage: number
 }
 
-export type PaginateQuery<T = any> = FilterQuery<T>
 export interface PaginateOptions {
   /** paginate options */
   page?: number
@@ -25,55 +24,62 @@ export interface PaginateOptions {
   projection?: string | object | null
   /** mongoose queryOptions */
   sort?: QueryOptions['sort']
-  lean?: QueryOptions['lean']
   populate?: QueryOptions['populate']
   /** original options for `model.find` */
   $queryOptions?: QueryOptions
 }
 
-const DEFAULT_OPTIONS: Required<Pick<PaginateOptions, 'page' | 'perPage' | 'dateSort' | 'lean'>> = Object.freeze({
+const DEFAULT_OPTIONS: Required<Pick<PaginateOptions, 'page' | 'perPage' | 'dateSort'>> = Object.freeze({
   page: 1,
   perPage: 16,
-  dateSort: -1,
-  lean: false
+  dateSort: -1 as const
 })
 
-export interface PaginateModel<T extends Document> extends Model<T> {
-  paginate(query?: PaginateQuery<T>, options?: PaginateOptions): Promise<PaginateResult<T>>
-}
-
-export function mongoosePaginate(schema: Schema) {
-  schema.statics.paginate = paginate
-}
-
-export function paginate<T>(this: Model<T>, filterQuery: PaginateQuery<T> = {}, options: PaginateOptions = {}) {
-  const { page, perPage, dateSort, projection, $queryOptions, ...resetOptions } = _merge(
+function doPaginate<TRawDocType>(
+  this: Model<TRawDocType>,
+  queryFilter: QueryFilter<TRawDocType> = {},
+  paginateOptions: PaginateOptions = {},
+  forceLean = false
+) {
+  const { page, perPage, dateSort, projection, $queryOptions, ...restOptions } = _merge(
     { ...DEFAULT_OPTIONS },
-    { ...options }
+    { ...paginateOptions }
   )
 
-  const findQueryOptions = {
-    ...resetOptions,
-    ...$queryOptions
+  const findQueryOptions: QueryOptions<TRawDocType> = {
+    ...restOptions,
+    ...$queryOptions,
+    lean: forceLean
   }
 
   // query
-  const countQuery = this.countDocuments(filterQuery).exec()
-  const pageQuery = this.find(filterQuery, projection, {
+  const countQuery = this.countDocuments(queryFilter).exec()
+  const listQuery = this.find(queryFilter, projection, {
     skip: (page - 1) * perPage,
     limit: perPage,
     sort: dateSort ? { _id: dateSort } : findQueryOptions.sort,
     ...findQueryOptions
   }).exec()
 
-  return Promise.all([countQuery, pageQuery]).then(([countResult, pageResult]) => {
-    const result: PaginateResult<T> = {
-      documents: pageResult,
-      total: countResult,
-      page,
-      perPage,
-      totalPage: Math.ceil(countResult / perPage) || 1
-    }
-    return result
-  })
+  return Promise.all([countQuery, listQuery]).then(([countResult, listResult]) => ({
+    documents: listResult,
+    total: countResult,
+    page,
+    perPage,
+    totalPage: Math.ceil(countResult / perPage) || 1
+  }))
+}
+
+export type PaginateStatics<TRawDocType, THydratedDocType = HydratedDocument<TRawDocType>> = {
+  paginate(filter?: QueryFilter<TRawDocType>, options?: PaginateOptions): Promise<PaginateResult<THydratedDocType>>
+  paginateRaw(filter?: QueryFilter<TRawDocType>, options?: PaginateOptions): Promise<PaginateResult<TRawDocType>>
+}
+
+export function mongoosePaginate(schema: Schema) {
+  schema.statics.paginate = function (this: any, filter?: any, options?: any) {
+    return doPaginate.call(this, filter, options, false)
+  }
+  schema.statics.paginateRaw = function (this: any, filter?: any, options?: any) {
+    return doPaginate.call(this, filter, options, true)
+  }
 }
